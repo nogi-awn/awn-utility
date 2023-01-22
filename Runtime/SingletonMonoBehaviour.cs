fb35ae5926e7bebfcd96c3294d22e014550241c8
nogi-awn <nogi.awn@gmail.com>
2023-01-22 16:39:58 +0900

    ステートマシンの追加

--- Runtime/SingletonMonoBehaviour.cs
+++ Runtime/SingletonMonoBehaviour.cs
@@ -2,17 +2,10 @@ using UnityEngine;
 
 namespace AwnUtility
 {
-    /// <summary>
-    /// 唯一のグローバルなアクセスポイントを提供するMonoBehaviour
-    /// </summary>
-    /// <typeparam name="T">このクラスの継承先</typeparam>
     public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
     {
         private static T instanceCache;
 
-        /// <summary>
-        /// インスタンスの参照。存在しない場合は検索・初期化して取得する
-        /// </summary>
         public static T instance
         {
             get
@@ -30,9 +23,6 @@ namespace AwnUtility
                 return instanceCache;
             }
         }
-        /// <summary>
-        /// シングルトンの初期化を行う関数。Awake時又はそれ以前にinstanceが参照された時点で呼び出される
-        /// </summary>
         protected virtual void Init() { }
 
         private void Awake()
---
+++ Runtime/StateMachine.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+
+namespace AwnUtility
+{
+    public class StateMachine<TOwner>
+    {
+        public abstract class State
+        {
+            protected StateMachine<TOwner> stateMachine => machine;
+            protected TOwner owner => machine.owner;
+
+            internal StateMachine<TOwner> machine;
+            internal Dictionary<int, State> transitions = new();
+
+            internal void Enter(State prevState)
+            {
+                OnEnter(prevState);
+            }
+            internal void Update()
+            {
+                OnUpdate();
+            }
+            internal void Exit(State nextState)
+            {
+                OnExit(nextState);
+            }
+
+            protected virtual void OnEnter(State prevState) {}
+            protected virtual void OnUpdate() {}
+            protected virtual void OnExit(State nextState) {}
+        }
+
+        public sealed class AnyState : State { }
+        public TOwner owner { get; }
+        public State currentState { get; private set; }
+
+        private LinkedList<State> stateList = new LinkedList<State>();
+
+        public StateMachine(TOwner owner)
+        {
+            this.owner = owner;
+        }
+
+        public void AddTransition<TFrom, TTo>(int triggerId)
+            where TFrom : State, new()
+            where TTo : State, new()
+        {
+            var from = GetOrAddState<TFrom>();
+            if(from.transitions.ContainsKey(triggerId))
+            {
+                throw new System.ArgumentException();
+            }
+            var to = GetOrAddState<TTo>();
+            from.transitions.Add(triggerId, to);
+        }
+
+        public void PullTrigger(int triggerId)
+        {
+            State to;
+            if(!currentState.transitions.TryGetValue(triggerId, out to))
+            {
+                if(!GetOrAddState<AnyState>().transitions.TryGetValue(triggerId, out to))
+                {
+                    return;
+                }
+            }
+            ChangeState(to);
+        }
+        public void Start<TFirst>()
+            where TFirst : State, new()
+        {
+            Start(GetOrAddState<TFirst>());
+        }
+        public void Start(State firstState)
+        {
+            currentState = firstState;
+            currentState.Enter(null);
+        }
+
+        public void Update() {
+            currentState.Update();
+        }
+
+        private T AddState<T>() where T : State, new()
+        {
+            var state = new T();
+            state.machine = this;
+            stateList.AddLast(state);
+
+            return state;
+        }
+        private T GetOrAddState<T>() where T : State, new()
+        {
+            foreach(var state in stateList)
+            {
+                if(state is T result)
+                    return result;
+            }
+            return AddState<T>();
+        }
+        private void ChangeState(State nextState)
+        {
+            currentState.Exit(nextState);
+            currentState.Enter(nextState);
+            currentState = nextState;
+        }
+    }
+}
